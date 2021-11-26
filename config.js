9684c5f529cca7665f53b27d7b9d248fc6c382e6
lehailong2007 <95010864+lehailong2007@users.noreply.github.com>
2021-11-26 09:57:00 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,19 +2,19 @@ const CONFIG = {
     titleWeb: "Chào em",
     introTitle: 'Yêu anh khum🥰',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
+    Mà sao anh cứ đi nhầm đường
+    Lạc vào tim em lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi❤️',
-    title: 'Dự báo thời tiết nói...Hôm nay có em 🥰',
+    title: 'Yêu Diệp không anh ớiiiii🥰',
     desc: 'Thoát ra hoặc không trả lời thì em làm ngiu anh :> ',
-    btnYes: 'Thích lắm <3',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://www.facebook.com/messages/t/100015646293297'
+    btnYes: 'Đéo :))',
+    btnNo: 'Yêu Diệp <3',
+    question: 'Tại sao anh khongg yêu Diệp :(',
+    btnReply: 'Gửi cho Diệp :<',
+    reply: 'Trông Diệp xấu vãi lồn :))',
+    mess: 'Haizz, buồn quá :((',
+    messDesc: 'Địt mẹ anh :).',
+    btnAccept: 'ấn vào để có in4',
+    messLink: 'https://www.facebook.com/diepanh.nguyen.92167789'
 }
